10a7b0e9626940b1e605457853358f46084c64f0
孟庆东 <781238222@qq.com>
2017-05-23 11:31:37 +0800

    add tabbarios

--- src/pages/components/TabBarIOSDemo.js
+++ src/pages/components/TabBarIOSDemo.js
@@ -6,6 +6,8 @@ import {
     StyleSheet,
     View,
     TabBarIOS,
+    Text,
+    Button,
 } from 'react-native';
 
 export default class TabBarIOSDemo extends Component {
@@ -13,21 +15,58 @@ export default class TabBarIOSDemo extends Component {
     static navigationOptions = {
         title: 'TabBarIOS',
     };
+    state = {
+        selectValue: 'tab1',
+        systemIcon:'bookmarks',
+    }
+    _onValueChange = (value) => {
+        this.setState({selectValue: value});
+    }
 
     render() {
         return (
-            <TabBarIOS>
+            <TabBarIOS
+                barTintColor='black'
+                tintColor="red"
+                unselectedItemTintColor="blue"
+                translucent={true}>
                 <TabBarIOS.Item
-                    title='tab1'
-                    icon={require('../../imgs/icon1.png')}>
-                        <View style={{flex:1,backgroundColor:'red'}}></View>
+                    badge='23'
+                    selected={this.state.selectValue == 'tab1'}
+                    systemIcon={this.state.systemIcon}
+                    onPress={()=>this._onValueChange('tab1')}>
+                    <View style={{flex:1}}>
+                        <View style={{flexDirection:'row',alignItems:'center',flexWrap:'wrap'}}>
+                            <Text>设置systemIcon：</Text>
+                            <Button title='bookmarks' onPress={()=>{this.setState({systemIcon:'bookmarks'})}}/>
+                            <Button title='contacts' onPress={()=>{this.setState({systemIcon:'contacts'})}}/>
+                            <Button title='downloads' onPress={()=>{this.setState({systemIcon:'downloads'})}}/>
+                            <Button title='favorites' onPress={()=>{this.setState({systemIcon:'favorites'})}}/>
+                            <Button title='featured' onPress={()=>{this.setState({systemIcon:'featured'})}}/>
+                            <Button title='history' onPress={()=>{this.setState({systemIcon:'history'})}}/>
+                            <Button title='more' onPress={()=>{this.setState({systemIcon:'more'})}}/>
+                            <Button title='most-recent' onPress={()=>{this.setState({systemIcon:'most-recent'})}}/>
+                            <Button title='most-viewed' onPress={()=>{this.setState({systemIcon:'most-viewed'})}}/>
+                            <Button title='recents' onPress={()=>{this.setState({systemIcon:'recents'})}}/>
+                            <Button title='search' onPress={()=>{this.setState({systemIcon:'search'})}}/>
+                            <Button title='top-rated' onPress={()=>{this.setState({systemIcon:'top-rated'})}}/>
+
+                        </View>
+                    </View>
                 </TabBarIOS.Item>
+
                 <TabBarIOS.Item
-                    title='tab2'>
+                    selected={this.state.selectValue == 'tab2'}
+                    icon={require('../../imgs/icon1.png')}
+                    title='tab2'
+                    onPress={()=>this._onValueChange('tab2')}>
                     <View style={{flex:1,backgroundColor:'blue'}}></View>
                 </TabBarIOS.Item>
+
                 <TabBarIOS.Item
-                    title='tab3'>
+                    selected={this.state.selectValue == 'tab3'}
+                    systemIcon='recents'
+                    onPress={()=>this._onValueChange('tab3')}>
                     <View style={{flex:1,backgroundColor:'green'}}></View>
                 </TabBarIOS.Item>
             </TabBarIOS>
